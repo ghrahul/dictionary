a8d9e60ec4591775b0eae73a85c9ec3dc076ffc3
ghrahul <ghrahul2016@gmail.com>
2018-01-19 00:35:20 +0530

    The basic program for searching a word in dictionary.This will run only in command line.

---
+++ dict.py
@@ -0,0 +1,43 @@
+import json
+import difflib
+from difflib import get_close_matches
+data = json.load(open("data.json"))
+def dict(q):
+    m = get_close_matches(q,data.keys())[0]
+    if q in data:
+        print("The meaning of the word is:\n ")
+        for item in data[q]:
+            print(item)
+    elif q.title() in data:
+        print("The meaning of the word is:\n ")
+        for item in data[q]:
+            print(item)
+
+    elif m in data:
+        answ =input("do you want to search this?y/n "+ m +" \n")
+        if answ=="y":
+            print("The meaning of the word is: \n")
+            for item in data[m]:
+                print(item)
+        elif answ=="n":
+            print("Sorry,The word doesn't exist in our dictionary.\n")
+        else:
+            print("Invalid response\n")
+    else:
+         print("The word doesn't exist.Please double check it\n")
+while(1):
+    ans = input("\nYou want to search something in dictionary?y/n \n")
+    if ans=="y":
+        query = input("give the word: \n")
+        dict(query.lower())
+    elif ans=="n":
+        print("Thank you for using\n")
+        break
+    else:
+        print("you gave invalid response\n")        
+
+      
+    
+
+
+
