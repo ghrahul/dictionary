efab66e8ab6b0fcd28a8567b95971791982ff04c
Rahul Ghosh <ghrahul2016@gmail.com>
2018-02-24 21:53:37 +0530

    Update dict.py
    efficient

--- dict.py
+++ dict.py
@@ -1,46 +1,27 @@
 import json
-import difflib
 from difflib import get_close_matches
+
 data = json.load(open("data.json"))
-def dict(q):
-    m = get_close_matches(q,data.keys())[0]
-    if q in data:
-        print("The meaning of the word is:\n ")
-        for item in data[q]:
-            print(item)
-    elif q.title() in data:
-        print("The meaning of the word is:\n ")
-        for item in data[q]:
-            print(item)
-    elif q.upper() in data:
-        print("The meaning of the word is:\n ")
-        for item in data[q.upper()]:
-            print(item)
-    elif m in data:
-        answ =input("do you want to search this?y/n "+ m +" \n")
-        if answ=="y":
-            print("The meaning of the word is: \n")
-            for item in data[m]:
-                print(item)
-        elif answ=="n":
-            print("Sorry,The word doesn't exist in our dictionary.\n")
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
         else:
-            print("Invalid response\n")
+            return "We didn't understand your entry."
     else:
-         print("The word doesn't exist.Please double check it\n")
-while(1):
-    ans = input("\nYou want to search something in dictionary?y/n \n")
-    if ans=="y":
-        query = input("give the word: \n")
-        dict(query.lower())
-    elif ans=="n":
-        print("Thank you for using\n")
-        break
-    else:
-        print("you gave invalid response\n")        
+        return "The word doesn't exist. Please double check it."
 
-      
-    
-
-
-
+word = input("Enter word: ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
